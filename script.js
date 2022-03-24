85c1a19f24b52948f7de0479e2df8666dea71c97
Míriam <m.feijoo.1995@gmail.com>
2022-03-24 13:19:53 +0100

    ej acabats

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // Estado de la APP
 let moveCat = false;
 let pixelsMove = 10;
-let volume = 0.5
+let volume = 0.5 // [0...1]
 let walkForwards = true;
+
+// Gestionar localStorage y sobreescribir las variables de estado que queramos
+if (localStorage.pixelsMove) {
+    pixelsMove = JSON.parse(localStorage.pixelsMove);
+    document.forms["catConf"].elements["velocidad"].value = pixelsMove;
+}
 
 // Variables globales
 const img = document.querySelector('img');
@@ -10,7 +16,20 @@ img.style.left = '0px';
 
 // Música!
 const audio = new Audio('./lambada.mp3');
+
+if (localStorage.moveCat) {
+    // JSON.parse va a convertir un string a un tipo de datos que entienda JavaScript; no solamente 'stirngs'
+    moveCat = JSON.parse(localStorage.moveCat);
+    audio.play();
+}
+
+if (localStorage.volume) {
+    volume = JSON.parse(localStorage.volume);
+    document.forms["catConf"].elements["volumen"].value = volume;
+}
+
 audio.volume = volume;
+
 
 // escuchar evento submit de formulario
 document.querySelector("form").addEventListener("submit", procesarValoresFormulario);
@@ -23,6 +42,15 @@ document.querySelector("form").addEventListener("submit", procesarValoresFormula
 // 3. Ejecutar el método .play del objeto 'audio'
 
 
+document.querySelector("#bailar").addEventListener("click", function () {
+    moveCat = true;
+    audio.play();
+
+    // Actualizar el localStorage para indicar que la próxima vez que carguemos la App el gato debería moverse
+    localStorage.moveCat = true;
+});
+
+
 
 // evento click al botón Parar!
 // Ejercicio 1
@@ -30,6 +58,12 @@ document.querySelector("form").addEventListener("submit", procesarValoresFormula
 // 1. Asociar el evento "click" al botón "Parar"
 // 2. Actualizar la variable de estado 'moveCat' a false
 // 3. Ejecutar el método .pause del objeto 'audio'
+
+document.querySelector("#parar").addEventListener("click", function () {
+    moveCat = false;
+    audio.pause();
+    localStorage.moveCat = false;
+});
 
 
 function catWalk() {
@@ -46,12 +80,12 @@ function catWalk() {
 
     }
 
-    // Ejercicio 4
+    // Ejercicio 3
 
     if (walkForwards) {
-        img.style.left = (currentLeft + 10) + 'px';
+        img.style.left = (currentLeft + pixelsMove) + 'px';
     } else {
-        img.style.left = (currentLeft - 10) + 'px';
+        img.style.left = (currentLeft - pixelsMove) + 'px';
     }
 }
 
@@ -65,12 +99,29 @@ function procesarValoresFormulario(event) {
     const velocidad = document.forms["catConf"].elements["velocidad"].value;
     console.log("nueva velocidad", velocidad);
 
+    // De los formularios siempre se recupera un stirng. Hay que transformarlo en number 
+
+    pixelsMove = parseInt(velocidad);
+    // pixelsMove = +velocidad;
+
     // Ejercicio 4
+
+    localStorage.pixelsMove = pixelsMove;
 
     // Ejercicio 5
 
+    // 1. Tomar el valor del input "velocidad". Cuidado!! Vaís a recuperar de nuevo un string
+    // 2. Actualizar la propiedad audio.volume
+    // 3. Establecer una propiedad del localStorage para guardar el volumen seleccionado por el usuario
+    // 4. AL INICIO de la aplicación, gestionar de manera similar el localStorage para cargar, o no, el volumen guardado
+
+    volume = document.forms["catConf"].elements["volumen"].value;
+    audio.volume = volume;
+    localStorage.volume = volume;
+
 }
 
+// Ejecuta esta función cada 50 ms
 
 setInterval(function () {
     // Ejercicio 1: Comprobar una variable de estado aquí y hacer un return inmediatamente sería una buena opción; si dicha variable nos dice que el gato no debe moverse.
